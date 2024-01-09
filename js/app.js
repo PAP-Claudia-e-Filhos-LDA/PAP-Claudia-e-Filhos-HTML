575e030f1c6dc60713e193aaad0fba76549975d9
rafael17cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2024-01-09 23:06:33 +0000

    responsive design

--- js/app.js
+++ js/app.js
@@ -1,29 +1,13 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.control');
-
-function PageTransitions() {
-  sectBtns.forEach(btn => {
-    btn.addEventListener('click', function() {
-      const id = this.getAttribute('data-id');
-
-      // Remove a classe 'active-btn' de todos os botões
-      document.querySelectorAll('.active-btn').forEach(activeBtn => {
-        activeBtn.classList.remove('active-btn');
-      });
-
-      // Adiciona a classe 'active-btn' ao botão clicado
-      this.classList.add('active-btn');
-
-      // Remove a classe 'active' de todas as seções
-      sections.forEach(section => {
-        section.classList.remove('active');
-      });
-
-      // Adiciona a classe 'active' à seção correspondente ao botão clicado
-      const selectedSection = document.getElementById(id);
-      selectedSection.classList.add('active');
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
     });
-  });
-}
-
-PageTransitions();
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
+  })
+})();
