bec6e83914ad0d804683d25fbcda97ec98fe4fc9
rafael17cordeiro <59150464+rafael17cordeiro@users.noreply.github.com>
2024-02-20 01:12:36 +0000

    ok

--- includes/process_encomenda.php
+++ includes/process_encomenda.php
@@ -8,20 +8,23 @@ $cartDetails = isset($_SESSION['cartDetails']) ? $_SESSION['cartDetails'] : [];
 $postData = file_get_contents("php://input");
 $postData = json_decode($postData, true);
 
-$tipoRissois = $postData['rissois'];
-$levantamento = $postData['levantamento'];
-$metodoPagamento = $postData['pagamento'];
-$mensagem = isset($postData['mensagem']) ? $postData['mensagem'] : "";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $tipoRissois = $_POST['rissois'];
+    $levantamento = $_POST['levantameto'];
+    $metodoPagamento = $_POST['pagamento'];
+    $mensagem = isset($_POST['mensagem']) ? $_POST['mensagem'] : "";
 
-echo "Tipo de Rissois: $tipoRissois<br>";
-echo "Levantamento: $levantamento<br>";
-echo "Método de Pagamento: $metodoPagamento<br>";
-echo "Mensagem: $mensagem<br>";
+    echo "Tipo de Rissois: $tipoRissois<br>";
+    echo "Levantamento: $levantamento<br>";
+    echo "Método de Pagamento: $metodoPagamento<br>";
+    echo "Mensagem: $mensagem<br>";
 
 
 
-foreach ($cartDetails as $item) {
-    echo "Produto: " . $item['title'] . ", Quantidade: " . $item['quantity'] . "<br>";
+
+    foreach ($cartDetails as $item) {
+        echo "" . $item['title'] . "," . $item['quantity'] . "<br>";
+    }
+} else {
+    header("location: ../php/index.php");
 }
-
-// Lembre-se de validar e processar os dados conforme necessário.
